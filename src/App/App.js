ecde77b702b2c56ab60b13243bad9d75bc9790e0
Beth <bethhnielsen56@gmail.com>
2020-05-18 21:32:40 -0500

    wip, logout button not showing

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,52 @@
 import React from 'react';
-import './App.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from './helpers/data/connections';
 
 import Auth from '../components/Auth/Auth';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 import PlayersContainer from '../components/PlayersContainer/PlayersContainer';
 
+import './App.scss';
+
+fbConnection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <PlayersContainer />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App">
         <MyNavbar/>
         <h2>INSIDE APP COMPONENT</h2>
-        <Auth/>
-        <PlayersContainer/>
+        {loadComponent()}
       </div>
     );
   }
--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  };
+
   render() {
+    const { authed } = this.props;
     return (
       <div className="MyNavbar">
-        <h1>Navbar</h1>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Sports Roster</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                {
+                  authed
+                    ? <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>
+                    : ''
+                }
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
