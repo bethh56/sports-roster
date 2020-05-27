d27299d093c93cdc8fdaa99c809053adcbceccca
Beth <bethhnielsen56@gmail.com>
2020-05-26 20:29:56 -0500

    linked up and working

---
+++ src/components/PlayerForm/PlayerForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './PlayerForm.scss';
+
+class PlayerForm extends React.Component {
+  render() {
+    return (
+      <div>
+        Checking
+      </div>
+    );
+  }
+}
+
+export default PlayerForm;
--- src/components/PlayersContainer/PlayersContainer.js
+++ src/components/PlayersContainer/PlayersContainer.js
@@ -4,11 +4,13 @@ import './PlayersContainer.scss';
 import playerData from '../../App/helpers/data/playerData';
 import authData from '../../App/helpers/data/authData';
 
+import PlayerForm from '../PlayerForm/PlayerForm';
 import Players from './Players/Players';
 
 class PlayersContainer extends React.Component {
   state = {
     sportsRoster: [],
+    formOpen: false,
   }
 
   getPlayerInfo = () => {
@@ -27,12 +29,21 @@ class PlayersContainer extends React.Component {
       .catch((err) => console.error('unable to delete player', err));
   }
 
+  addPlayerEvent = (e) => {
+    e.preventDefault();
+    console.error('button');
+  }
+
   render() {
-    const { sportsRoster } = this.state;
+    const { sportsRoster, formOpen } = this.state;
     const viewPlayers = sportsRoster.map((player) => <Players key={player.id} player={player} deletePlayer={this.deletePlayer}/>);
     return (
+      <div>
+      <button className="btn btn-success mt-2" onClick={() => this.setState({ formOpen: true })}>Add Player</button>
+      { formOpen ? <PlayerForm /> : '' }
       <div className="PlayersContainer d-flex flex-wrap justify-content-center">
         {viewPlayers}
+      </div>
       </div>
     );
   }
